0b538c4ff58ddbe37e5b89618c4645d3ecc2b17c
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-12 02:38:05 -0400

    created: featured products layout

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from 'react';
+import { useProductsContext } from '../context/products_context';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Error from './Error';
+import Loading from './Loading';
+import Product from './Product';
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>
-}
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext();
+  if (loading) {
+    return <Loading />;
+  }
+
+  // Handling error
+  if (error) {
+    return <Error />;
+  }
+
+  return (
+    <Wrapper className='section'>
+      <div className='title'>
+        <h2>featured products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center featured'>
+        {/* Get only 3 products */}
+        {featured.slice(1, 4).map((product) => {
+          return <Product key={product.id} {...product} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
@@ -31,6 +58,6 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
-`
+`;
 
-export default FeaturedProducts
+export default FeaturedProducts;
