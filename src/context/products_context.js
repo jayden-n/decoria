3143578671c60da89708c08ab8755cc134da775c
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-12 01:50:12 -0400

    added: fetching products info API

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -31,6 +31,14 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
+  const fetchProducts = async (url) => {
+    const response = await axios.get(url);
+    console.log(response);
+  };
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
+
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
