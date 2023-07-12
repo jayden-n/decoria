6d7ee8a530fc1217fc8b2e3daac109e53eaa369f
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-12 02:16:27 -0400

    refactored: fetching products API logic completed

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,12 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+
+  // Products
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -31,10 +37,18 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
+  // Fetching products API
   const fetchProducts = async (url) => {
-    const response = await axios.get(url);
-    console.log(response);
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
+
   useEffect(() => {
     fetchProducts(url);
   }, []);
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,7 +16,23 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
-
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
