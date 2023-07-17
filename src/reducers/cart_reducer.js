27cff6d80ba820f6f4411f4a7a0ea2318f8778db
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-17 17:38:47 -0400

    added: AddToCart reducer - new item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,28 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from '../actions';
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((i) => i.id === id + color);
+    if (tempItem) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.image[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
 
-export default cart_reducer
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
