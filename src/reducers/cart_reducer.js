a5075a6e690eee9a84c570449a55c28988687957
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-17 17:50:42 -0400

    added: AddToCart reducer - existing item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -11,6 +11,21 @@ const cart_reducer = (state, action) => {
     const { id, color, amount, product } = action.payload;
     const tempItem = state.cart.find((i) => i.id === id + color);
     if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+
+          // adding product amount to cart depends on the product stocks left
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
+
+      return { ...state, cart: tempCart };
     } else {
       const newItem = {
         id: id + color,
