ae69e8435a6fbdd09ca3452144fc3b0a802ae414
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-18 15:13:07 -0400

    added: cart page content structure

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,18 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions';
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -29,6 +39,10 @@ export const CartProvider = ({ children }) => {
   const toggleAmount = (id, value) => {};
   // clear cart
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider
