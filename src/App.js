7486552f0427644edb4e7df69175c5b2fbff44fe
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-24 22:09:25 -0400

    fixed: auth0 - when user is logged in and wants to navigate to checkout page [BUG FIXED]

--- src/App.js
+++ src/App.js
@@ -11,36 +11,39 @@ import {
   About,
   Products,
   PrivateRoute,
+  AuthWrapper,
 } from './pages';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
-        <Route exact path='/products'>
-          <Products />
-        </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />} />
-        <PrivateRoute exact path='/checkout'>
-          <Checkout />
-        </PrivateRoute>
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <Route exact path='/products'>
+            <Products />
+          </Route>
+          <Route exact path='/products/:id' children={<SingleProduct />} />
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
