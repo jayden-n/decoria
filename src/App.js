cb6d9c4c20f495a8f81d1a225916c303acc9b930
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-25 23:55:14 -0400

    refactored: react router 6 upgrade

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
-
 import {
   Home,
   SingleProduct,
@@ -13,34 +12,28 @@ import {
   PrivateRoute,
   AuthWrapper,
 } from './pages';
-
 function App() {
   return (
     <AuthWrapper>
       <Router>
         <Navbar />
         <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/cart'>
-            <Cart />
-          </Route>
-          <Route exact path='/products'>
-            <Products />
-          </Route>
-          <Route exact path='/products/:id' children={<SingleProduct />} />
-          <PrivateRoute exact path='/checkout'>
-            <Checkout />
-          </PrivateRoute>
-          <Route path='*'>
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+          <Route path='*' element={<Error />} />
+        </Routes>
         <Footer />
       </Router>
     </AuthWrapper>
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   const { user } = useAuth0();
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        return user ? children : <Redirect to='/' />;
-      }}
-    ></Route>
-  );
+  if (!user) {
+    return <Navigate to='/' />;
+  }
+  return children;
 };
 export default PrivateRoute;
